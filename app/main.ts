b6fd68a6fb0b59c20da791c62225f2d0e7cd0717
Adams Banjo <sajioloye@gmail.com>
2024-09-28 19:22:30 -0400

    return server response

--- app/main.ts
+++ app/main.ts
@@ -5,9 +5,14 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on("connect", () => {
+    console.log("Client connected");
+  });
   socket.on("close", () => {
     socket.end();
   });
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.pipe(socket);
 });
 
 server.listen(4221, "localhost");
